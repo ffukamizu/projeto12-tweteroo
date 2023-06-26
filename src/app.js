03fa402ff5ad23e35cbeb1ac2da9ea83b5ad3eb3
ffukamizu <f.fukamizu@live.com>
2023-06-26 14:57:02 -0300

    Implemented user sign-up

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express, { json } from 'express';
 import cors from 'cors';
-
+const port = 5000;
 const app = express();
+
 app.use(json());
 app.use(cors());
+
+//global users and tweets
+const userList = [];
+// const tweetList = [];
+
+app.post('/sign-up', (req, res) => {
+    const {username, avatar} = req.body;
+
+    userList.push({ username, avatar });
+    res.status(201).send('Ok');
+})
+
+app.listen(port, (() => console.log(`Server is online, utilizing port: ${port}`)));
